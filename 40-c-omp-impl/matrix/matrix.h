e9ba865487a3b7445ededea94b09201c2dcbada5
jolyons <john.lyons.93@hotmail.com>
2022-01-08 18:41:39 +0100

    Refactor changes

--- 40-c-omp-impl/matrix/matrix.h
+++ 40-c-omp-impl/matrix/matrix.h
@@ -33,13 +33,13 @@ typedef struct matrix_mult_operation{
 matrix create_matrix(long rows, long cols);
 void free_matrix(matrix* mat);
 void matrix_random_init(matrix* mat, float max);
-int prepare_matrix_mult(matrix* A, matrix* B, matrix* C, int row_split, int col_split, matrix_mult_operation* mult_op);
+int prepare_matrix_block_mult(matrix* A, matrix* B, matrix* C, int row_split, int col_split, matrix_mult_operation* mult_op);
 void close_matrix_mult(matrix_mult_operation* mult_op);
 void print_matrix(char name, matrix* mat, long row_split, long col_split, long max_len);
 void print_split_matrix(char name, split_matrix* mat, long max_len);
 void sub_matrix_mul(matrix_mult_operation* mul_op, sub_matrix_meta* A, sub_matrix_meta* B);
 void matrix_simple_init(matrix* mat);
-void matrix_mul(matrix_mult_operation* mult_op);
+void matrix_block_mul(matrix_mult_operation* mult_op);
 
 // matrix operations
 #define MIDX(r, c, w) (w * r + c)
